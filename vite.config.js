efae23a47ad7c5a5facc3264a5adaa91308ee69b
dkhawk <107309+dkhawk@users.noreply.github.com>
2026-06-12 16:36:35 -0600

    build(web): configure IIFE output format for static file loading

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'iife',
+      },
+    },
+  },
 });
